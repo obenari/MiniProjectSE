70c630368256ee724eb5c93a1acfd0d8f8878185
obenari <obenari@g.jct.ac.il>
2021-03-15 17:24:27 +0200

    initial commit

---
+++ src/primitives/Point3D.java
@@ -0,0 +1,13 @@
+package primitives;
+
+public class Point3D {
+    final Coordinate _x;
+    final Coordinate _y;
+    final Coordinate _z;
+
+    public Point3D(Coordinate x, Coordinate y, Coordinate z) {
+        _x = x;
+        _y = y;
+        _z = z;
+    }
+}
