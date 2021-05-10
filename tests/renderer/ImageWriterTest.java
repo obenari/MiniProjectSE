1a18843f40beea169ad3bba9ebd346e75912e2ad
talya535@gmail.com <talya535@gmail.com>
2021-05-10 22:54:51 +0300

    המשך תרגיל 5

---
+++ tests/renderer/ImageWriterTest.java
@@ -0,0 +1,31 @@
+package renderer;
+
+import elements.Camera;
+import org.junit.jupiter.api.Test;
+import primitives.Color;
+import primitives.Point3D;
+import primitives.Vector;
+import scene.Scene;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageWriterTest {
+
+    @Test
+    void testWriteToImage() {
+        ImageWriter imageWriter=new ImageWriter("module check 1", 800,500);
+        Scene scene=new Scene("module check 1");
+        scene.setBackground(new Color(0,0,255));
+        Camera camera=new Camera(Point3D.ZERO,new Vector(0,0,1), new Vector(1,0,0))
+                .setDistance(2)
+                .setViewPlaneSize(800, 500);
+        Render render = new Render() //
+                .setImageWriter(imageWriter) //
+                .setCamera(camera) //
+                .setRayTracer(new BasicRayTracer(scene));
+
+        render.renderImage();
+        render.printGrid(50, new Color(java.awt.Color.PINK));
+        render.writeToImage();
+    }
+}
--- tests/renderer/RenderTests.java
+++ tests/renderer/RenderTests.java
@@ -40,7 +40,7 @@ public class RenderTests {
                 new Triangle(new Point3D(100, 0, -100), new Point3D(0, -100, -100), new Point3D(100, -100, -100))); // down
         // right
 
-        ImageWriter imageWriter = new ImageWriter("base render test", 1000, 1000);
+        ImageWriter imageWriter = new ImageWriter("base render test1", 1000, 1000);
         Render render = new Render() //
                 .setImageWriter(imageWriter) //
                 .setCamera(camera) //
